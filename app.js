dfaa0a07a1a74faaccf82a0735edd09d147233bb
Angel Chen <ychen297@ncsu.edu>
2025-05-09 14:22:29 -0400

    buttons added

---
+++ app.js
@@ -0,0 +1,10 @@
+const money_savings=205.02
+const money_checking=571.90
+const money_cash=471
+
+const view_ledger=document.querySelector("#viewledger")
+const view_debts=document.querySelector("#viewdebts")
+
+view_debts.addEventListener("click",()=>{
+    document.querySelector("#debts").style.display="none"
+})
